a2445f3dbe96187c7f87b74c7f03fa8035358510
root <root@zitpcx18435.desy.de>
2015-08-31 13:01:23 +0200

    create pool in run-dcache rather than in the Dockerfile

--- dcache/master/standalone/run-dcache.sh
+++ dcache/master/standalone/run-dcache.sh
@@ -8,6 +8,11 @@ stopDcache() {
 
 ${DCACHE} database update
 
+if [ ! -d /pools/pools ]
+then
+  dcache pool create --size=10000000000 /pools/pools pool1 pools
+fi
+
 ${DCACHE} start
 
 lock=$(${DCACHE} property dcache.paths.lock.file)
